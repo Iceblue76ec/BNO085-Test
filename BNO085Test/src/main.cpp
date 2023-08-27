fd07c5da03ab4a63bce973e7195c92cb594d0d94
iceblue76ec <1430013882@qq.com>
2023-08-27 21:15:34 +0800

    修改了一下，让校准变得更加直观了

--- BNO085Test/src/main.cpp
+++ BNO085Test/src/main.cpp
@@ -27,116 +27,18 @@
   Serial.print it out at 115200 baud to serial monitor.
 */
 
-//选择用途
-#define Test true      // 用于测试改为true
-#define Calibrate false // 用于校准改为true
-
-//测试部分
-#if Test
-#include <Wire.h>
-
-#include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
-BNO080 myIMU;
-
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("BNO08X Read Example\n");
-
-  // 在这里修改(SDA,ACL)
-  Wire.begin(4, 5); // slime官方默认nodemcu接线
-  // Wire.begin(D2, D3); // 磷桦板子
-
-  Serial.printf("[INFO]  imu begin\n");
-
-  while (1)
-  {
-    if (myIMU.begin(0x4A, Wire))
-    {
-      Serial.printf("[INFO]  BNO08X at 0X4A Successful initialization.\n");
-      break;
-    }
-    else if (myIMU.begin(0x4B, Wire))
-    {
-      Serial.printf("[INFO]  BNO08X at 0X4B Successful initialization.\n");
-      break;
-    }
-    else
-    {
-      Serial.printf("[ERR!]  BNO08X at 0X4A/0X4B initialization failure.\n");
-      Serial.printf("[INFO]  Wait 13 seconds to try again.\n");
-      delay(13000);
-    }
-  }
-
-  Wire.setClock(400000); // Increase I2C data rate to 400kHz
-  Serial.printf("[INFO]  Increase I2C data rate to 400kHz\n");
-
-  myIMU.enableAccelerometer(50); // Send data update every 50ms
-  Serial.printf("[INFO]  enable Accelerometer\n");
-  myIMU.enableGyro(50); // Send data update every 50ms
-  Serial.printf("[INFO]  enable Gyro\n");
-  myIMU.enableMagnetometer(50); // Send data update every 50ms
-  Serial.printf("[INFO]  enable Magnetometer\n\n");
-  myIMU.enableRotationVector(50); // Send data update every 50ms
-  Serial.printf("[INFO]  enable RotationVector\n\n");
-  myIMU.enableDebugging(Serial); // Output debug messages to the Serial port. Serial1, SerialUSB, etc is also allowed.
-
-  // Serial.println(F("Output in form x, y, z, in m/s^2"));
-  // Serial.println(F("Output in form x, y, z, in radians per second"));
-  // Serial.println(F("Output in form x, y, z, in uTesla"));
-  delay(2000);
-}
-
-void loop()
-{
-  // Look for reports from the IMU
-  if (myIMU.dataAvailable() == true)
-  {
-    float ax = myIMU.getAccelX();
-    float ay = myIMU.getAccelY();
-    float az = myIMU.getAccelZ();
-
-    float gx = myIMU.getGyroX();
-    float gy = myIMU.getGyroY();
-    float gz = myIMU.getGyroZ();
-
-    float mx = myIMU.getMagX();
-    float my = myIMU.getMagY();
-    float mz = myIMU.getMagZ();
-
-    float roll = (myIMU.getRoll()) * 180.0 / PI;   // Convert roll to degrees
-    float pitch = (myIMU.getPitch()) * 180.0 / PI; // Convert pitch to degrees
-    float yaw = (myIMU.getYaw()) * 180.0 / PI;     // Convert yaw / heading to degrees
-
-    Serial.print(roll, 1);
-    Serial.print(F(","));
-    Serial.print(pitch, 1);
-    Serial.print(F(","));
-    Serial.print(yaw, 1);
-    Serial.printf("加速度计Acceleration(m/s^2):[%5.3lf,%5.3lf,%5.3lf]\n", ax, ay, az);
-    Serial.printf("陀螺仪Gyroscope(rad/s):     [%5.3lf,%5.3lf,%5.3lf]\n", gx, gy, gz);
-    Serial.printf("磁力计Magnetometer(uTesla): [%5.3lf,%5.3lf,%5.3lf]\n", mx, my, mz);
-    Serial.printf("欧拉角roll,pitch,yaw:       [%3lf,%3lf,%3lf]\n", roll, pitch, yaw);
-    myIMU.printPacket();
-    Serial.println();
-
-    delay(250);
-  }
-}
-#endif
-
-#if CaCalibrate
 /*
-这后面的代码时用于085校准的，如果不是完全异常、几分钟就自旋了40度以上的085，否则不要轻易校准。
+这里的的代码时用于085校准的，如果不是完全异常、几分钟就自旋了40度以上的085，否则不要轻易校准。
 校准步骤请参阅手册
 */
 
 #include <Wire.h>
-
 #include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
 BNO080 myIMU;
+
+// 程序时钟
+const unsigned long interval = 150; // 串口打印输出间隔为150毫秒
+unsigned long previousMillis = 0;   // 上一次输出的时间
 
 // Given a accuracy number, print what it means
 void printAccuracyLevel(byte accuracyNumber)
@@ -150,22 +52,18 @@ void printAccuracyLevel(byte accuracyNumber)
   else if (accuracyNumber == 3)
     Serial.print(F("High"));
 }
+
 void setup()
 {
+
   Serial.begin(115200);
   Serial.println();
-  Serial.println("BNO080 Read Example");
 
-  // Wire.begin();
+  // 在这里修改你板子对应的的SDA,SCL引脚
+  // Wire.begin(4, 5); // 自用
+  //  Wire.begin(D2, D1); // 赤灵用的
+  // Wire.begin(D2, D3); // 磷桦PCB
 
-  // myIMU.begin();
-
-  /**
-   * 加上自己的部分
-   * */
-  // Wire.begin(4, 5); // 自己用的
-  Wire.begin(D2, D1); // 赤灵用的
-  // myIMU.begin(0x4A, Wire);//自己用的
   while (1)
   {
     if (myIMU.begin(0x4A, Wire))
@@ -181,32 +79,55 @@ void setup()
     else
     {
       Serial.printf("[ERR!]  BNO08X at 0X4A/0X4B initialization failure.\n");
-      Serial.printf("[INFO]  Wait 13 seconds to try again.\n");
-      delay(13000);
+      Serial.printf("[INFO]  Wait 10 seconds to try again.\n");
+      Serial.printf("[INFO]  启动失败，请手动断电重启085\n");
+      delay(10000);
+      // ESP.restart();没啥效果
     }
   }
 
   Wire.setClock(400000); // Increase I2C data rate to 400kHz
 
-  // Enable dynamic calibration for accel, gyro, and mag
-  myIMU.calibrateAll(); // Turn on cal for Accel, Gyro, and Mag
+  myIMU.enableAccelerometer(50); // Send data update every 50ms
+  Serial.printf("[INFO]  enable Accelerometer\n");
+
+  myIMU.enableGyro(50); // Send data update every 50ms
+  Serial.printf("[INFO]  enable Gyro\n");
+
+  myIMU.enableMagnetometer(50); // Send data update every 50ms
+  Serial.printf("[INFO]  enable Magnetometer\n\n");
+
+  myIMU.enableRotationVector(50); // Send data update every 50ms
+  Serial.printf("[INFO]  enable RotationVector\n\n");
+
+  myIMU.calibrateAll();
 
   // Enable Game Rotation Vector output
   myIMU.enableGameRotationVector(100); // Send data update every 100ms
 
-  // Enable Magnetic Field output
-  myIMU.enableMagnetometer(100); // Send data update every 100ms
+  // Serial.println("---------注意事项：-------------");
+  // Serial.println("校准过程中，请仔细观察每个部件的精度值。");
+  // Serial.println("全部校准到hign后,输入 s 保存校准数据到085芯片内");
+  // Serial.println("保存后将关闭自动校准");
+  // Serial.println("结束后请刷回原本SlimeVR固件");
 
-  // Once magnetic field is 2 or 3, run the Save DCD Now command
-  Serial.println(F("Calibrating. Press 's' to save to flash"));
-  Serial.println(F("Output in form x, y, z, in uTesla"));
 }
 
 void loop()
 {
+  unsigned long currentMillis = millis(); // 获取当前时间
+
   if (Serial.available())
   {
     byte incoming = Serial.read();
+
+    if (incoming == 'c')
+    {
+      // Enable dynamic calibration for accel, gyro, and mag
+      myIMU.calibrateAll(); // Turn on cal for Accel, Gyro, and Mag
+      Serial.println("重新启动自动校准");
+      delay(1000);
+    }
 
     if (incoming == 's')
     {
@@ -238,7 +159,9 @@ void loop()
         Serial.println("Calibration data failed to store. Please try again.");
       }
 
-      // myIMU.endCalibration(); //Turns off all calibration
+      myIMU.endCalibration(); // Turns off all calibration
+      Serial.println("关闭自动校准");
+      delay(1000);
       // In general, calibration should be left on at all times. The BNO080
       // auto-calibrates and auto-records cal data roughly every 5 minutes
     }
@@ -247,10 +170,20 @@ void loop()
   // Look for reports from the IMU
   if (myIMU.dataAvailable() == true)
   {
-    float x = myIMU.getMagX();
-    float y = myIMU.getMagY();
-    float z = myIMU.getMagZ();
-    byte accuracy = myIMU.getMagAccuracy();
+    float ax = myIMU.getAccelX();
+    float ay = myIMU.getAccelY();
+    float az = myIMU.getAccelZ();
+    byte AccelAccuracy = myIMU.getAccelAccuracy();
+
+    float gx = myIMU.getGyroX();
+    float gy = myIMU.getGyroY();
+    float gz = myIMU.getGyroZ();
+    byte GyroAccuracy = myIMU.getGyroAccuracy();
+
+    float mx = myIMU.getMagX();
+    float my = myIMU.getMagY();
+    float mz = myIMU.getMagZ();
+    byte MagAccuracy = myIMU.getMagAccuracy();
 
     float quatI = myIMU.getQuatI();
     float quatJ = myIMU.getQuatJ();
@@ -258,29 +191,26 @@ void loop()
     float quatReal = myIMU.getQuatReal();
     byte sensorAccuracy = myIMU.getQuatAccuracy();
 
-    Serial.print(x, 2);
-    Serial.print(F(","));
-    Serial.print(y, 2);
-    Serial.print(F(","));
-    Serial.print(z, 2);
-    Serial.print(F(","));
-    printAccuracyLevel(accuracy);
-    Serial.print(F(","));
+    if (currentMillis - previousMillis >= interval)
+    {                                 // 判断是否满足输出条件
+      previousMillis = currentMillis; // 更新上一次输出的时间
 
-    Serial.print("\t");
-
-    Serial.print(quatI, 2);
-    Serial.print(F(","));
-    Serial.print(quatJ, 2);
-    Serial.print(F(","));
-    Serial.print(quatK, 2);
-    Serial.print(F(","));
-    Serial.print(quatReal, 2);
-    Serial.print(F(","));
-    printAccuracyLevel(sensorAccuracy);
-    Serial.print(F(","));
-
-    Serial.println();
+      // 每秒20次打印串口信息
+      Serial.println("---BNO08X---------原始数据----------精度-------");
+      Serial.printf("Accel加速度计:  [%.2f,%.2f,%.2f] ,", ax, ay, az);
+      printAccuracyLevel(AccelAccuracy);
+      Serial.println();
+      Serial.printf("Gyro陀螺仪:     [%.2f,%.2f,%.2f] ,", gx, gy, gz);
+      printAccuracyLevel(GyroAccuracy);
+      Serial.println();
+      Serial.printf("Mag磁力计:      [%.2f,%.2f,%.2f] ,", mx, my, mz);
+      printAccuracyLevel(MagAccuracy);
+      Serial.println();
+      Serial.printf("Quat欧拉角:      [%.2f,%.2f,%.2f,%.2f] ,", quatI, quatJ, quatK, quatReal);
+      printAccuracyLevel(sensorAccuracy);
+      Serial.println();
+      Serial.println();
+      Serial.println();
+    }
   }
 }
-#endif
